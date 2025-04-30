850522986a92dc7061123b072c32c5db7fc616f5
alessmcs <alessandramancas23@gmail.com>
2025-04-30 15:48:59 -0400

    partie 1

--- TP2_code/main.py
+++ TP2_code/main.py
@@ -5,10 +5,16 @@ import numpy as np
 from sklearn import linear_model
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.neural_network import MLPClassifier
+from sklearn import metrics
 import matplotlib
 import transformers
 import re  # regex library
+
+from transformers import model_addition_debugger
+
 
 ### PARTIE 1 ###
 
@@ -28,39 +34,71 @@ def bag_of_words(content):
     return df
 
 # TF-IDF
-def tf_idf(content):
+def tf_idf(contents):
     vectorizer = TfidfVectorizer(max_features=5000)
-    count_matrix = vectorizer.fit_transform(content)
+    count_matrix = vectorizer.fit_transform(contents)
     count_array = count_matrix.toarray()
     df = pd.DataFrame(data=count_array, columns=vectorizer.get_feature_names_out())
     return df
 
 # Entraîner modèles (Logistic regression, Random Forest, MLP)
 # https://thecleverprogrammer.com/2024/02/19/compare-multiple-machine-learning-models/
+def train_model(model, inputs, labels, test_inputs, test_labels):
+    # x : input, y: label
+    classifier = model
+    classifier.fit(inputs, labels)
 
+    y_pred = classifier.predict(test_inputs)
+    print(y_pred)
+
+    # tout calculer p/r à l'ensemble de test
+    scores = cross_val_score(classifier, test_inputs, test_labels, cv=5)
+    accuracy = accuracy_score(test_labels, y_pred)
+    f1_score = metrics.f1_score(test_labels, y_pred)
+
+    return scores, accuracy, f1_score
 
 
 
 def main():
 
-    df = pd.read_csv('spam_train.csv', header=None, names=['text', 'label'])
+    df = pd.read_csv('spam_train.csv')
 
     # Preprocess the data
     df['text'] = pretraitement(df['text'])
-    # print(df.head())
+    df['label'] = pd.to_numeric(df['label'], downcast='integer', errors='coerce')
+    print(df.head())
+
 
     fm = bag_of_words(df['text'])
+    fm_train, fm_test, y1_train, y1_test = train_test_split(fm, df['label'], random_state=42)
     # print(fm.head)
 
     tfidf = tf_idf(df['text'])
-    print(tfidf.head)
+    tfidf_train, tfidf_test, y2_train, y2_test = train_test_split(tfidf, df['label'], random_state=42)
+    # print(tfidf_train.head)
 
     # todo: choose parameters
     models = {
-        "LR": linear_model.LinearRegression(),
+        "LR": linear_model.LogisticRegression(),
         "RF": RandomForestClassifier(),
         "MLP": MLPClassifier()
     }
+
+    for model in models:
+        print("training model " + model)
+
+        classifier = models[model]
+        bow_scores, bow_accuracy, bow_f1 = train_model(classifier, fm_train, y1_train, fm_test, y1_test)
+        tfidf_scores, tfidf_accuracy, tfidf_f1 = train_model(classifier, tfidf_train, y2_train, tfidf_test, y2_test)
+
+        print('bow scores', bow_scores, 'avg')
+        print('bow accuracy', bow_accuracy)
+        print('bow f1', bow_f1, '\n')
+
+        print('tfidf scores', tfidf_scores)
+        print('tfidf accuracy', tfidf_accuracy)
+        print('tfidf f1', tfidf_f1, '\n')
 
 
 # Entry point
